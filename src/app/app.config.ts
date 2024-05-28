c555ff59519dd4140ba45e70d830e4ad584e9c45
Erik Norfelt <erik.norfelt@monitor.se>
2024-05-28 08:42:55 +0200

    provide http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
     provideRouter(routes) 
   ]
 };
