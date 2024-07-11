64e5de9a25b68cd9718cfd77ff968e33fd87a9ed
Erik Norfelt <erik.norfelt@monitor.se>
2024-07-11 15:33:23 +0200

    ✨ Actions menu

--- src/app/components/actions/actions.component.spec.ts
+++ src/app/components/actions/actions.component.spec.ts
@@ -1,16 +1,84 @@
-import { render } from "@testing-library/angular";
+import { render, screen } from "@testing-library/angular";
 import { ActionsComponent } from "./actions.component";
+import userEvent from "@testing-library/user-event";
+import { ScoreBoardStore } from "../../core/state/score-board.store";
 
 describe('ActionsComponent', () => {
   it('should render', async () => {
-    const { fixture } = await render(ActionsComponent);
+    const { fixture } = await renderComponent();
 
-    expect(fixture).toBeTruthy(); 
+    expect(fixture).toBeTruthy();
   });
-  
-  const renderComponent = async () => {
-    const { fixture } = await render(ActionsComponent);
 
-    return { fixture}    
+  it('should not show menu by default', async () => {
+    const { menu } = await renderComponent();
+
+    expect(menu()).not.toBeInTheDocument();
+  });
+
+  it('should toggle menu', async () => {
+    const { menu, toggleMenu } = await renderComponent();
+
+    await toggleMenu();
+    expect(menu()).toBeInTheDocument();
+
+    await toggleMenu();
+    expect(menu()).not.toBeInTheDocument();
+  });
+
+  it('should close menu when clicking outside', async () => {
+    const { menu, toggleMenu, clickBody, debug } = await renderComponent();
+
+    await toggleMenu();
+    expect(menu()).toBeInTheDocument();
+
+    await clickBody();
+    expect(menu()).not.toBeInTheDocument();
+  });
+
+  it('should call reset when clicked', async () => {
+    const { clickReset, store, toggleMenu } = await renderComponent();
+
+    await toggleMenu();
+    await clickReset();
+
+    expect(store.reset).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call forceUpdate when clicked', async () => {
+    const { clickForceUpdate, store, toggleMenu } = await renderComponent();
+
+    await toggleMenu();
+    await clickForceUpdate();
+
+    expect(store.forceUpdate).toHaveBeenCalledTimes(1);
+  });
+
+  const renderComponent = async () => {
+    const user = userEvent.setup();
+
+    const scoreBoardStoreMock = {
+      reset: jest.fn(),
+      forceUpdate: jest.fn()
+    };
+
+    const { fixture, debug } = await render(ActionsComponent, {
+      providers: [
+        { 
+          provide: ScoreBoardStore, 
+          useValue: scoreBoardStoreMock }
+      ]
+    });
+
+    return {
+      fixture,
+      debug,
+      store: scoreBoardStoreMock,
+      menu: () => screen.queryByRole('menu'),
+      toggleMenu: () => user.click(screen.getByRole('button', { name: /actions/i })),
+      clickBody: () => user.click(document.body),
+      clickReset: () => user.click(screen.getByRole('menuitem', { name: /reset/i })),
+      clickForceUpdate: () => user.click(screen.getByRole('menuitem', { name: /force update/i })),
+    }
   }
-});
+}); 
--- src/app/components/actions/actions.component.ts
+++ src/app/components/actions/actions.component.ts
@@ -1,12 +1,32 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, inject, signal } from "@angular/core";
+import { ClickOutsideDirective } from "../../directives/click-outside/click-outside.directive";
+import { ScoreBoardStore } from "../../core/state/score-board.store";
 
 @Component({
   selector: 'app-actions',
   templateUrl: './actions.component.html',
   styleUrls: ['./actions.component.scss'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ClickOutsideDirective]
 })
 export class ActionsComponent {
+  private store = inject(ScoreBoardStore);
+  showMenu = signal(false)
 
+  toggleMenu() {
+    this.showMenu.update(value => !value)
+  }
+
+  closeMenu() {
+    this.showMenu.set(false);
+  }
+
+  reset() {
+    this.store.reset();
+  }
+
+  forceUpdate() {
+    this.store.forceUpdate();
+  }
 }
