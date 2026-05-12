62ea6eed52055179c205867ffa4c9602335097ec
Erik Norfelt <erik.norfelt@monitor.se>
2026-05-12 11:00:12 +0200

    removed build warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,36 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { SectionComponent } from './components/section/section.component';
-import { ErrorDialogComponent } from './components/error-dialog/error-dialog.componet';
-import { AddRemoveButtonsComponent } from './components/inc-dec-buttons/inc-dec-buttons.component';
-import { SegmentDisplayComponent } from './components/segment-display/segment-display.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
-import { ScoreBoardStore } from './core/state/score-board.store';
-import { StatusComponent } from './components/status/status.component';
-import { ActionsComponent } from './components/actions/actions.component';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+import { SectionComponent } from "./components/section/section.component";
+import { ErrorDialogComponent } from "./components/error-dialog/error-dialog.componet";
+import { AddRemoveButtonsComponent } from "./components/inc-dec-buttons/inc-dec-buttons.component";
+import { SegmentDisplayComponent } from "./components/segment-display/segment-display.component";
+import { SpinnerComponent } from "./components/spinner/spinner.component";
+import { ScoreBoardStore } from "./core/state/score-board.store";
+import { StatusComponent } from "./components/status/status.component";
+import { ActionsComponent } from "./components/actions/actions.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        CommonModule,
-        RouterOutlet,
-        SectionComponent,
-        SegmentDisplayComponent,
-        AddRemoveButtonsComponent,
-        SpinnerComponent,
-        ErrorDialogComponent,
-        StatusComponent,
-        ActionsComponent
-    ],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-root",
+  imports: [
+    CommonModule,
+    SectionComponent,
+    SegmentDisplayComponent,
+    AddRemoveButtonsComponent,
+    SpinnerComponent,
+    ErrorDialogComponent,
+    StatusComponent,
+    ActionsComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   private store = inject(ScoreBoardStore);
@@ -38,12 +43,18 @@ export class AppComponent {
 
   firstOutSig = computed(() => {
     const state = this.store.state();
-    return (state.outsInInning > 0 && state.outsInInning < 3) || (state.outsInInning > 3);
+    return (
+      (state.outsInInning > 0 && state.outsInInning < 3) ||
+      state.outsInInning > 3
+    );
   });
 
   secondOutSig = computed(() => {
     const state = this.store.state();
-    return (state.outsInInning > 1 && state.outsInInning < 3) || (state.outsInInning > 4);
+    return (
+      (state.outsInInning > 1 && state.outsInInning < 3) ||
+      state.outsInInning > 4
+    );
   });
 
   homeScoreIncreased() {
